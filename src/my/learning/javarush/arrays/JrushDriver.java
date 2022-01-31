ce1727525c23041cbb86517c4bb49b2c98d2fccd
nikolay <veselovnd@gmail.com>
2022-01-31 18:37:16 +0300

    jrushtasks

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -1,15 +1,13 @@
 package my.learning.javarush.arrays;
 
 
+import java.util.Arrays;
 
 public class JrushDriver {
 
 
     public static void main(String[] args) {
-        String path = "/usr/java/jdk1.8/bin/";
-
-        String jdk13 = "jdk-13";
-        System.out.println(PlaywStrings.changePath(path, jdk13));
+        System.out.println(PlaywStrings.reverseString("Hate"));
 
 }
 }
--- src/my/learning/javarush/arrays/PlaywStrings.java
+++ src/my/learning/javarush/arrays/PlaywStrings.java
@@ -1,5 +1,7 @@
 package my.learning.javarush.arrays;
 
+import java.util.StringTokenizer;
+
 public class PlaywStrings {
 
     public static int getIndexFromFirstWord(String string, String word) {
@@ -16,5 +18,21 @@ public class PlaywStrings {
         int index2 = path.indexOf("/", index);
         String result = path.replaceAll(path.substring(index,index2), jdk);
         return result;
+    }
+    public static  String[] getTokens(String query, String delimiter){
+        StringTokenizer tkn = new StringTokenizer(query, delimiter);
+        String[] result = new String[tkn.countTokens()];
+        for (int i = 0; tkn.hasMoreTokens(); i++) {
+            result[i] = tkn.nextToken();
+        }
+            return result;
+        }
+
+    public static boolean equal(String first, String second){
+        return first.intern()==second.intern();
+    }
+    public static String reverseString(String string){
+        StringBuilder sb = new StringBuilder(string);
+        return sb.reverse().toString();
     }
 }
