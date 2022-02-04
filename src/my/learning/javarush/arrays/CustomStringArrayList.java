a2bf40a1185f63a605296c50cf87e64ce357c129
Nikolay <veselovnd@gmail.com>
2022-02-04 16:01:57 +0300

    jrush

---
+++ src/my/learning/javarush/arrays/CustomStringArrayList.java
@@ -0,0 +1,33 @@
+package my.learning.javarush.arrays;
+
+public class CustomStringArrayList {
+
+    private int size;
+    private int capacity;
+    private String[] elements;
+
+    public CustomStringArrayList() {
+        capacity = 10;
+        size = 0;
+        elements = new String[capacity];
+    }
+
+    public void add(String element) {
+        if (size == capacity) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        capacity = (int) (capacity*1.5);
+        String[] temp = new String[(capacity)];
+        for (int i = 0; i<elements.length; i++){
+            temp[i] = elements[i];
+        }
+        elements = temp;
+
+
+    }
+}
--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -6,32 +6,14 @@ import java.util.Arrays;
 
 public class JrushDriver {
 
-    public static int[] numbers = new int[] {56, 45, 89, 1, 45, 13, 4, 69, 11, -89};
+
 
     public static void main(String[] args) {
-        print();
-        sort();
-        System.out.println("\nОтсортированные числа в натуральном порядке:");
-        print();
-    }
-
-    public static void sort() {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            for (int j = 0; j < numbers.length - i - 1; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
-
-                }
-            }
+        CustomStringArrayList arrayList = new CustomStringArrayList();
+        for(int i = 0; i < 25; i++) {
+            arrayList.add("count" + i);
         }
     }
 
-    public static void print() {
-        for (int number : numbers) {
-            System.out.print(number + "\t");
-        }
-    }
 }
 
