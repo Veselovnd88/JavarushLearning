93f7ee70605f732424007fb32a4205656b2b0fa3
Nikolay <veselovnd@gmail.com>
2022-02-04 21:40:10 +0300

    converters

---
+++ src/my/learning/javarush/arrays/Collectionstraining.java
@@ -0,0 +1,40 @@
+package my.learning.javarush.arrays;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Collectionstraining {
+    public static void reverse(ArrayList<Integer> list) {
+        Collections.reverse(list);
+    }
+
+    public static void sort(ArrayList<Integer> list) {
+    Collections.sort(list);
+    }
+
+    public static void rotate(ArrayList<Integer> list, int distance) {
+Collections.rotate(list,distance);
+    }
+
+    public static void shuffle(ArrayList<Integer> list) {
+    Collections.shuffle(list);
+}
+    public static Integer min(ArrayList<Integer> list) {
+        return Collections.min(list);
+    }
+
+    public static Integer max(ArrayList<Integer> list) {
+
+        return Collections.max(list);
+    }
+
+    public static int frequency(ArrayList<Integer> list, Integer element) {
+
+        return Collections.frequency(list, element);
+    }
+
+    public static int binarySearch(ArrayList<Integer> list, Integer key) {
+        Collections.sort(list);
+        return Collections.binarySearch(list,key);
+    }
+}
--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -3,17 +3,23 @@ package my.learning.javarush.arrays;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class JrushDriver {
-
-
-
     public static void main(String[] args) {
-        CustomStringArrayList arrayList = new CustomStringArrayList();
-        for(int i = 0; i < 25; i++) {
-            arrayList.add("count" + i);
-        }
+
+    }
+    public static void copy(ArrayList<String> destination, ArrayList<String> source) {
+        Collections.copy(destination,source);
     }
 
+    public static void addAll(ArrayList<String> list, String... strings) {
+        Collections.addAll(list,strings);
+    }
+
+    public static void replaceAll(ArrayList<String> list, String oldValue, String newValue) {
+        Collections.replaceAll(list, oldValue,newValue);
+    }
 }
 
