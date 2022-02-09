62991771203f9cfbf00063353697acde055ad326
Nikolay <veselovnd@gmail.com>
2022-02-09 21:09:26 +0300

    converters

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -1,22 +1,27 @@
 package my.learning.javarush.arrays;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class JrushDriver {
     public static void main(String[] args) {
-        StringsLinkedList stringsLinkedList = new StringsLinkedList();
-        stringsLinkedList.add("1");
-        stringsLinkedList.add("2");
-        stringsLinkedList.add("3");
-        stringsLinkedList.add("4");
-        stringsLinkedList.add("5");
-        stringsLinkedList.add("6");
-        stringsLinkedList.add("7");
-        stringsLinkedList.add("8");
-        stringsLinkedList.add("9");
-        //stringsLinkedList.printAll();
-        System.out.println(stringsLinkedList.get(4)); // 5
+        try (Scanner scanner = new Scanner(System.in);
+             var inputStream = Files.newInputStream(Paths.get(scanner.nextLine()));
+             var outputStream = Files.newOutputStream(Paths.get(scanner.nextLine()))
+        ) {
+            int size = 1;
+            byte[] buffer = new byte[size];
+            while (inputStream.available() > 0) {
+                int read = inputStream.read(buffer);
+                outputStream.write(buffer, 0, read);
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong : " + e);
+        }
     }
 
 
