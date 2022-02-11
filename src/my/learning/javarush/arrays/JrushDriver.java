95860e8191aefd8cd5d89ef7d5c008f65c470e2b
nikolay <veselovnd@gmail.com>
2022-02-11 17:44:09 +0300

    jrushtasks

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -1,15 +1,38 @@
 package my.learning.javarush.arrays;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class JrushDriver {
 
 
+    public static ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    public static PrintStream stream = new PrintStream(outputStream);
 
-    public static void main(String[] args) {
-    PlayIO.ex5();
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        printSomething(scanner.nextLine());
+        String result = outputStream.toString();
+        outputStream.reset();
+        byte[] arr = result.getBytes();
+        for (int i = 0; i <arr.length/2; i++) {
+            byte tmp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = tmp;
+
+        }
+        outputStream.write(arr);
+        System.out.println(outputStream.toString());
+
+    }
+
+    public static void printSomething(String str) {
+        stream.print(str);
     }
 
 }
