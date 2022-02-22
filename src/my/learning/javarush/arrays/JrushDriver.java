a0a441a0b1901ac63225fe44cbc8a2ca9acb3fe7
Nikolay <veselovnd@gmail.com>
2022-02-22 22:03:01 +0300

    tasks

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -5,26 +5,12 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Stream;
-
+import java.time.Month;
 public class JrushDriver {
 
     public static void main(String[] args) {
-        ArrayList<Language> languages = new ArrayList<>();
-        Collections.addAll(languages,
-                new Language("С#", 4.58),
-                new Language("JavaScript", 2.54),
-                new Language("Python", 10.47),
-                new Language("C++", 7.11),
-                new Language("Go", 1.46),
-                new Language("R", 2.37),
-                new Language("Java", 13.48),
-                new Language("C", 15.95),
-                new Language("Swift", 1.38),
-                new Language("PHP", 2.49));
-
-        Stream<Language> languageStream = Language.sortByRanking(languages);
-
-        languageStream.forEach(System.out::println);
+        LocalDate d = LocalDate.of(2022, Month.MARCH, 12);
+        System.out.println(d.isAfter(LocalDate.now()));
     }
 
 
