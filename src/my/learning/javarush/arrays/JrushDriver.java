fdfa0cf7d2a95b652c3f9518de204cdea4e3d534
Nikolay <veselovnd@gmail.com>
2022-02-23 17:46:57 +0300

    tasks

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -4,13 +4,17 @@ package my.learning.javarush.arrays;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.time.Month;
 public class JrushDriver {
 
     public static void main(String[] args) {
-        LocalDate d = LocalDate.of(2022, Month.MARCH, 12);
-        System.out.println(d.isAfter(LocalDate.now()));
+      ArrayList<String> list = new ArrayList<>();
+      Collections.addAll(list,"Hate", "World","Disaster");
+      Stream<String> s = list.stream();
+      List<String> l = s.filter(x->(x.length()<5)).collect(Collectors.toList());
+      l.forEach(System.out::println);
     }
 
 
