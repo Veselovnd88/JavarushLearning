7c6ea7f5ddfe911f2583735ebf09ab4346425fff
Nikolay <veselovnd@gmail.com>
2022-02-28 21:47:14 +0300

    tasks

--- src/my/learning/javarush/arrays/JrushDriver.java
+++ src/my/learning/javarush/arrays/JrushDriver.java
@@ -12,7 +12,33 @@ import java.time.Month;
 public class JrushDriver {
 
     public static void main(String[] args) {
+        System.out.println(Matrix.NEO);
+    }
 
+    static class Matrix {
+        public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
+    }
+
+    interface DBObject {
+        DBObject initializeIdAndName(long id, String name);
+    }
+
+    static class User implements DBObject {
+        long id;
+        String name;
+
+
+        @Override
+        public String toString() {
+            return String.format("The user's name is %s, id = %d", name, id);
+        }
+
+        @Override
+        public User initializeIdAndName(long id, String name) {
+            this.id = id;
+            this.name = name;
+            return this;
+        }
     }
 
 
