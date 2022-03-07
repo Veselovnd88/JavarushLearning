59df3ea927bbb6af72e291586f7e564ab6689d6f
Nikolay <veselovnd@gmail.com>
2022-03-07 13:03:06 +0300

    tasks

--- src/my/learning/javarush/st2/st2Driver.java
+++ src/my/learning/javarush/st2/st2Driver.java
@@ -2,6 +2,6 @@ package my.learning.javarush.st2;
 
 public class st2Driver {
     public static void main(String[] args) throws Exception {
-        BooksTask.task();
+        InitTasks.task();
     }
 }
