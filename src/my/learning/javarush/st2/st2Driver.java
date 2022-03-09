73e91d03cd2315d4d160fbe78cded6fbc385ada4
nikolay <veselovnd@gmail.com>
2022-03-09 18:36:09 +0300

    dukeTask

--- src/my/learning/javarush/st2/st2Driver.java
+++ src/my/learning/javarush/st2/st2Driver.java
@@ -1,6 +1,8 @@
 package my.learning.javarush.st2;
 
-
+import my.learning.javarush.st2.planes.PlanesTask;
+import my.learning.javarush.st2.planets.PlanetsTask;
+import org.w3c.dom.ls.LSOutput;
 
 import java.math.BigDecimal;
 import java.util.Scanner;
@@ -9,6 +11,18 @@ public class st2Driver {
 
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        Test t = new Test();
+        Thread thr = new Thread(t);
+        Thread thr2 = new Thread(t);
+        thr.start();
+        thr.join();
+        thr2.start();
     }
-}
+    public static class Test implements  Runnable{
+        public void run(){
+        for (int i = 0; i<20; i++){
+            System.out.println(i);
+        }
+    }
+}}
+
