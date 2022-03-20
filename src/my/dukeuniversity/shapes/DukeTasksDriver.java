a398492037d45ecff4f5063950f82915e1f98fca
nikolay <veselovnd@gmail.com>
2022-03-20 13:45:42 +0300

    jrushtasks

--- src/my/dukeuniversity/shapes/DukeTasksDriver.java
+++ src/my/dukeuniversity/shapes/DukeTasksDriver.java
@@ -1,9 +1,10 @@
 package my.dukeuniversity.shapes;
 
+import my.dukeuniversity.shapes.csv.CSVtemp;
 import my.dukeuniversity.shapes.csv.ExampleCsv;
 
 public class DukeTasksDriver {
     public static void main(String[] args) {
-        ExampleCsv.task();
+        CSVtemp.task();
     }
 }
--- src/my/dukeuniversity/shapes/csv/CSVtemp.java
+++ src/my/dukeuniversity/shapes/csv/CSVtemp.java
@@ -7,8 +7,9 @@ import java.io.File;
 public class CSVtemp {
 
     public static void task(){
-        FileResource fr = new FileResource("src/my/dukeuniversity/shapes/csv/nc_weather/2015/weather-2015-01-07.csv");
-        CS
+        //FileResource fr = new FileResource("src/my/dukeuniversity/shapes/csv/nc_weather/2015/weather-2015-01-07.csv");
+        CSVRecord c = hottestInManyDays();
+        System.out.println(c.get("TemperatureF")+":"+ c.get("DateUTC"));
     }
     public static CSVRecord hottestHourInFile(CSVParser parser){
         CSVRecord largestSoFar = null;
@@ -26,5 +27,25 @@ public class CSVtemp {
             }
         } return largestSoFar;
     }
+    public static CSVRecord hottestInManyDays(){
+        CSVRecord largestSoFar = null;
+        DirectoryResource dr = new DirectoryResource();
+        for(File f: dr.selectedFiles()){
+            FileResource fr = new FileResource(f);
+            CSVRecord current = hottestHourInFile(fr.getCSVParser());
+            largestSoFar = getLargestOfTwo(current,largestSoFar);
+            }return largestSoFar;
+        }
+    public static CSVRecord getLargestOfTwo(CSVRecord current, CSVRecord largestSoFar){
+        if(largestSoFar==null){
+            largestSoFar = current;
+        }
+        else {
+            double currTemp = Double.parseDouble(current.get("TemperatureF"));
+            double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+            if(currTemp>largestTemp){
+                largestSoFar = current;
+            }
+    } return largestSoFar;}
 
 }
