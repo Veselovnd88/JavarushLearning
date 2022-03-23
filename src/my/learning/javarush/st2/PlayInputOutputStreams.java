12b91ecdff6978d10a2d1677de8bba3f23bbe697
nikolay <veselovnd@gmail.com>
2022-03-23 18:30:04 +0300

    jrushtasks

--- src/my/learning/javarush/st2/PlayInputOutputStreams.java
+++ src/my/learning/javarush/st2/PlayInputOutputStreams.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class PlayInputOutputStreams {
 
     public static void task(){
-        oftenBytes();
+        commas();
 
     }
     public static void training(){
@@ -90,6 +90,79 @@ public class PlayInputOutputStreams {
         } catch (Exception e){
             e.printStackTrace();
 
+        }
+    }
+    public static void rareBytes(){
+        //src/my/learning/javarush/arrays/in.txt
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        int count = 0;
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String file = br.readLine();
+            FileInputStream fis = new FileInputStream(file);
+            while (fis.available()>0){
+                count++;
+                int bt = fis.read();
+                if (hm.containsKey(bt)){
+                    hm.put(bt, hm.get(bt)+1);
+                }
+                else {
+                    hm.put(bt,1);
+                }
+            } int min=Integer.MAX_VALUE;
+            for (Map.Entry<Integer,Integer> en: hm.entrySet()){
+                if (en.getValue()<min && en.getValue()!=0){
+                    min = en.getValue();
+                }
+            }
+            for (Map.Entry<Integer, Integer> en :hm.entrySet()){
+                if( en.getValue()==min){
+                    System.out.print(en.getKey()+" ");
+                }
+            }
+            fis.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void sortBytes(){
+        Set<Integer> bytesSet = new TreeSet<>();
+
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String file = br.readLine();
+            FileInputStream fis = new FileInputStream(file);
+            while(fis.available()>0){
+                int bt = fis.read();
+                bytesSet.add(bt);
+            }
+            bytesSet.forEach(x-> System.out.print(x+" "));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void commas(){
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String file = br.readLine();
+            FileInputStream fis = new FileInputStream(file);
+            int count = 0;
+            while (fis.available()>0){
+                if(fis.read()==44){
+                    count++;
+                }
+            }fis.close();
+            System.out.println(count);
+
+        }
+        catch ( Exception e){
+            e.printStackTrace();
         }
     }
 }
