42c14d6e1c25b727c3d4682cc81fa6c669ae7329
Nikolay <veselovnd@gmail.com>
2022-04-06 21:30:47 +0300

    jrush tasks

---
+++ src/my/learning/javarush/st2/bonustasks/Crossword.java
@@ -0,0 +1,61 @@
+package my.learning.javarush.st2.bonustasks;
+
+import java.util.List;
+
+public class Crossword {
+    public static void task() {
+        int[][] crossword = new int[][]{
+                {'f', 'd', 'e', 'r', 'l', 'k'},
+                {'u', 's', 'a', 'm', 'e', 'o'},
+                {'l', 'n', 'g', 'r', 'o', 'v'},
+                {'m', 'l', 'p', 'r', 'r', 'h'},
+                {'p', 'o', 'e', 'e', 'j', 'j'}
+        };
+        detectAllWords(crossword, "home", "same");
+        /*
+Ожидаемый результат
+home - (5, 3) - (2, 0)
+same - (1, 1) - (4, 1)
+         */
+    }
+
+    public static List<Word> detectAllWords(int[][] crossword, String... words) {
+        for (String s:words){
+            for(int i=0; i<s.length();i++){
+                for(int j=0; j<crossword.length;i++){
+
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public static class Word {
+        private String text;
+        private int startX;
+        private int startY;
+        private int endX;
+        private int endY;
+
+        public Word(String text) {
+            this.text = text;
+        }
+
+        public void setStartPoint(int i, int j) {
+            startX = i;
+            startY = j;
+        }
+
+        public void setEndPoint(int i, int j) {
+            endX = i;
+            endY = j;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s - (%d, %d) - (%d, %d)", text, startX, startY, endX, endY);
+        }
+    }
+
+}
