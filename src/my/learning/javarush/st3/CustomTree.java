3d373006951c29e29cd44a0b44e1041fa7dd805e
Nikolay <veselovnd@gmail.com>
2022-04-12 15:13:12 +0300

    jrush

--- src/my/learning/javarush/st3/CustomTree.java
+++ src/my/learning/javarush/st3/CustomTree.java
@@ -2,11 +2,72 @@ package my.learning.javarush.st3;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class CustomTree extends AbstractList<String> implements Serializable,Cloneable {
+    public Entry<String> root;
+    public List<Entry> listTree = new ArrayList<>();
 
+    public CustomTree(){
+        this.root = new Entry<>("name");
+        listTree.add(root);
+
+    }
+    public String getParent(String s){
+        for (Entry e: listTree){
+            if(e.elementName!=null){
+                if(e.elementName.equals(s)){
+                    return e.parent.elementName;
+                }
+            }
+        }return null;
+    }
+
+    @Override
+    public boolean add(String s) {
+        for (Entry e: listTree){
+            if (e.isAvailableToAddChildren()){//у нас есть рут, если у него ветки установлены в тру
+                Entry<String> newElem = new Entry<>(s);//создаем новый элемент
+                newElem.parent = e;//устанавливаем родителя
+                if (e.availableToAddLeftChildren){//проверяем левую ветвь
+
+                    e.leftChild = newElem;// устанавливаем в левую ветвь
+                    e.availableToAddLeftChildren = false;//ставим фолс на лвую ветвь
+                    listTree.add(newElem);// добавили в наш список
+                    return true;
+                } else if (e.availableToAddRightChildren) {
+                    e.rightChild = newElem;
+                    e.availableToAddRightChildren = false;
+                    listTree.add(newElem);
+                    return true;
+                }
+            }
+        } return false;
+    }
+
+
+
+    static class Entry<T> implements  Serializable{
+        String elementName;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+        Entry <T> parent;
+        Entry<T> leftChild;
+        Entry<T> rightChild;
+
+        public Entry (String s)  {
+            this.elementName = s;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren(){
+            return this.availableToAddLeftChildren || this.availableToAddRightChildren;
+        }
+
+    }
     @Override
     public String get(int index) {
         throw new UnsupportedOperationException();
@@ -44,7 +105,7 @@ public class CustomTree extends AbstractList<String> implements Serializable,Clo
 
     @Override
     public int size() {
-        return 0;
+        return listTree.size()-1;
     }
 
 
--- src/my/learning/javarush/st3/CustomTreeTask.java
+++ src/my/learning/javarush/st3/CustomTreeTask.java
@@ -5,5 +5,16 @@ import java.util.List;
 public class CustomTreeTask {
     public static void task(String[] args){
         List<String> list = new CustomTree();
+
+
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        ((CustomTree) list).listTree.forEach(x->{
+            System.out.println(x.elementName);
+        });
+        System.out.println("The list size is " + list.size());
+        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
     }
 }
