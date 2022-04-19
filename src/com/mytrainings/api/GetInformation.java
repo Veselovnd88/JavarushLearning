67b026457758ef71476d24ea10b66735765c104a
Nikolay <veselovnd@gmail.com>
2022-04-19 16:44:56 +0300

    jrush

--- src/com/mytrainings/api/GetInformation.java
+++ src/com/mytrainings/api/GetInformation.java
@@ -1,5 +1,6 @@
 package com.mytrainings.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import my.learning.javarush.st2.DebugTask;
 
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class GetInformation {
@@ -15,29 +17,49 @@ public class GetInformation {
     private String link = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
     private String result;
 
-    public String getInfo() throws IOException {
-        if (result!=null){
-        URL obj = new URL(this.link);
-        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
-        conn.setRequestMethod("GET");
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String input;
-        StringBuilder sb = new StringBuilder();
-        while(br.ready()){
-            sb.append(br.readLine());
-        } result = sb.toString();
+    public String getInfo(){
+        if (result==null){
+
+        try{
+            URL obj = new URL(this.link);
+            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+            conn.setRequestMethod("GET");
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String input;
+            StringBuilder sb = new StringBuilder();
+            while(br.ready()){
+                sb.append(br.readLine());
+        } result = sb.toString();}
+        catch (MalformedURLException  e){
+            e.printStackTrace();
+
+            System.out.println("Что-то не так со ссылкой");
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+            System.out.println("Что-то пошло не так");
+        }
         }
 
         //System.out.println(sb.toString());
         return result;
     }
 
-    public Image getImageObj() throws IOException {
+    public Image getImageObj() {
 
         ObjectMapper om = new ObjectMapper();
+        try{
         Image ob = om.readValue(getInfo(), Image.class);
+            return ob;}
+        catch (IllegalArgumentException | JsonProcessingException e){
+            System.out.println("Не удалось открыть ссылку");
+            e.printStackTrace();
+            }
+        return null;
 
-        return ob;
+
 
     }
 
