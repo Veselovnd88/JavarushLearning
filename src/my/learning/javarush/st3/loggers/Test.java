665582019747eb061b2b39a5f7e060f1ac0622b3
Nikolay <veselovnd@gmail.com>
2022-04-26 11:17:58 +0300

    jrush

--- src/my/learning/javarush/st3/loggers/Test.java
+++ src/my/learning/javarush/st3/loggers/Test.java
@@ -1,12 +1,18 @@
 package my.learning.javarush.st3.loggers;
+import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test {
     private static final Logger logger = LoggerFactory.getLogger(Test.class);
+    //файл проперти должен быть в папке src
+    // добавил зависимости через Maven
+    //org.slf4j:slf4j-log4j12
+    //org.slf4j:slf4j-api
+
     public void check(String srt){
         try{
-        logger.debug(srt+" передана");}
+        logger.trace(srt+" передана");}
         catch (Exception e){
             logger.error("Error");
         }
