105da5a354336cf4182a9b56b7cd34040059be66
nikolay <veselovnd@gmail.com>
2022-05-30 21:23:38 +0300

    jrushtasks

--- src/my/learning/javarush/st3/agreggator/model/HHStrategy.java
+++ src/my/learning/javarush/st3/agreggator/model/HHStrategy.java
@@ -3,6 +3,8 @@ package my.learning.javarush.st3.agreggator.model;
 import my.learning.javarush.st3.agreggator.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -23,9 +25,17 @@ public class HHStrategy implements Strategy{
         //if(searchString==null){
          //   return Collections.emptyList();
        // }
-        final String userAgent = "Mozilla/5.0 (jsoup)";
+        final String userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)" +
+                " Chrome/101.0.4951.64 Safari/537.36 OPR/87.0.4390.25";
+        Document doc = null;
         try {
-            Document doc = Jsoup.connect(String.format(URL_FORMAT, "Moscow", 1)).get();
+            doc = Jsoup.connect("https://javarush.ru/testdata/big28data.html").userAgent(userAgent)
+                    .referrer("https://hh.ru").get();
+
+            Elements els = doc.getElementsByAttributeValue("data-qa","vacancy-serp__vacancy");
+            for(Element el:els){
+                System.out.println(el.getElementsByAttributeValue("data-qa","vacancy-serp__vacancy-title"));
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
