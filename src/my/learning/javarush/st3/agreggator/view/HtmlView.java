0dbf737b51b6343a22bb8a6f519fb9401d2f7bc7
nikolay <veselovnd@gmail.com>
2022-05-31 18:32:35 +0300

    jrushtasks

--- src/my/learning/javarush/st3/agreggator/view/HtmlView.java
+++ src/my/learning/javarush/st3/agreggator/view/HtmlView.java
@@ -2,15 +2,31 @@ package my.learning.javarush.st3.agreggator.view;
 
 import my.learning.javarush.st3.agreggator.Controller;
 import my.learning.javarush.st3.agreggator.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 public class HtmlView implements View{
     private Controller controller;
-
+    private final String filePath ="src/my/learning/javarush/st3/agreggator/view/vacancies.html";
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        try{
+            getDocument(filePath);
+        //String updated = getUpdatedFileContent(vacancies);
+        //updateFile(updated);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        //System.out.println(filePath);
+        //System.out.println(vacancies.size());
     }
 
     @Override
@@ -20,5 +36,21 @@ public class HtmlView implements View{
 
     public void userCitySelectEmulationMethod(){
         controller.onCitySelect("Moscow");
+    }
+    private String getUpdatedFileContent(List<Vacancy> vacancies){
+
+        return null;
+    }
+     private void updateFile(String string) throws IOException {
+         BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
+         bw.write(string);
+         bw.flush();
+         bw.close();
+    }
+    protected Document getDocument(String filePath) throws IOException {
+        File file = new File(filePath);
+        Document doc = Jsoup.parse(file,"UTF-8");
+        System.out.println(doc);
+        return doc;
     }
 }
