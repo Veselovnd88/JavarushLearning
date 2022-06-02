5d26d6f9ef24e7b03fa11d663707d6612ee8ca11
Nikolay <veselovnd@gmail.com>
2022-06-02 17:08:39 +0300

    jrush

---
+++ src/my/learning/javarush/st3/level9/oneChange.java
@@ -0,0 +1,57 @@
+package my.learning.javarush.st3.level9;
+
+public class oneChange {
+    public static void main(String[] args) {
+        System.out.println(isOneEditAway("abаcd", "abcd"));
+    }
+
+    public static boolean isOneEditAway(String first, String second){
+        if(first.equals(second)){
+            return true;
+        }//если одинаковые строки то возвращаем тру
+        if(first.length()-second.length()>1 || second.length()-first.length()>1){
+            return false;// если различие больше чем на 2 символа, возвращаем фолс
+        }
+        if(first.length()==second.length()){
+            char[] arr1 = first.toCharArray();
+            char[] arr2 = second.toCharArray();
+            int unmatched = 0;
+            for (int i=0; i<first.length(); i++){
+                if(arr1[i]!=arr2[i]){
+                    unmatched++;
+                }
+            }
+            if(unmatched>1){
+                return false;
+            }
+            return true;// проверяем если одинаковые длины - проходим по каждому символу и считаем нессответствия
+            //если больше одного - то фолс, если до 1 то тру
+        }
+        if(first.length()>second.length()){
+            char[] arr1 =second.toCharArray();//короткий
+            char[] arr2 = first.toCharArray();//длинный
+            int unmatched =0;
+            int p1=0;
+            int p2=0;
+            for(int i=0; i<second.length();i++){
+                if(arr2[i+p2]!=arr1[i+p1]){
+                    if(arr1[i]!=arr2[i+1]){
+                        return false;
+                    }
+                    unmatched++;
+                    p2++;
+                }
+
+                }if(unmatched>1){
+                    return false;
+
+
+            } return true;
+
+        }
+
+
+
+    return false;
+    }
+}
