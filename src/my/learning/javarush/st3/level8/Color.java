7f58c5de47b277462a78606783a36942447f790f
Nikolay <veselovnd@gmail.com>
2022-06-03 16:25:01 +0300

    jrush tasks

---
+++ src/my/learning/javarush/st3/level8/Color.java
@@ -0,0 +1,11 @@
+package my.learning.javarush.st3.level8;
+
+public enum Color {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    INDIGO,
+    VIOLET
+}
---
+++ src/my/learning/javarush/st3/level8/PhotoPaint.java
@@ -0,0 +1,33 @@
+package my.learning.javarush.st3.level8;
+
+public class PhotoPaint {
+    public boolean paintFill(Color[][] image, int x, int y, Color desiredColor) {
+        Color originalColor = image[y][x];
+
+        if(originalColor==desiredColor){
+            return false;
+        }
+        if(y>image.length|| x>image[0].length){
+            return false;
+        }
+
+
+
+            paintFill(image,x,y,desiredColor,originalColor);
+            return true;
+
+    }
+    public void paintFill(Color[][] image, int x, int y, Color desiredColor, Color oldColor){
+        if(x<0|| y<0||x>image[0].length||y>image.length){
+            return;
+        }
+        if(image[y][x]==oldColor){
+            image[y][x] = desiredColor;
+            paintFill(image,x+1,y,desiredColor,oldColor);
+            paintFill(image,x-1,y,desiredColor,oldColor);
+            paintFill(image,x,y+1,desiredColor,oldColor);
+            paintFill(image,x,y-1,desiredColor,oldColor);
+        }
+    }
+}
+
