3f29a77ee6d6868092c83a2cd3bd40431d885112
Nikolay <veselovnd@gmail.com>
2022-06-08 15:31:28 +0300

    jrush

--- src/my/learning/javarush/st3/url/Mails.java
+++ src/my/learning/javarush/st3/url/Mails.java
@@ -1,8 +1,5 @@
 package my.learning.javarush.st3.url;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.*;
 import java.io.FileReader;
@@ -19,20 +16,30 @@ public class Mails {
             String str = "src/"+(Mails.class.getPackage().getName()+".").replace(".","/")+"mail.properties";
             System.out.println(str);
 
-
+            String username = "suffocation666@inbox.ru";
+            String password = "";
             props.load(new FileReader(str));
-            Session session = Session.getDefaultInstance(props);
-            MimeMessage message = new MimeMessage(session);
+            Session session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                        @Override
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
 
+
+            MimeMessage message = new MimeMessage(session);
+            message.addFrom(new Address[]{new InternetAddress("suffocation666@inbox.ru")});
             message.setSubject("Привет");
             message.setText("Это Тестовое письмо");
-
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("veselovnd@gmail.com"));
             message.setSentDate(new Date());
 
 
 
-            Transport transport = session.getTransport();// авторизация
 
+            Transport transport = session.getTransport();// авторизация
+            transport.connect((String) props.get("mail.host"),465, (String)props.get("mail.user"),(String) props.get("mail.password"));
             transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
 
 
