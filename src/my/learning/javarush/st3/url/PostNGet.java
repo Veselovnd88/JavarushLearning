c4bb8bb2f67ba56aff747e534c83bdbd5d349c03
Nikolay <veselovnd@gmail.com>
2022-06-08 13:23:59 +0300

    jrush

--- src/my/learning/javarush/st3/url/PostNGet.java
+++ src/my/learning/javarush/st3/url/PostNGet.java
@@ -10,7 +10,11 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,8 +23,10 @@ import java.util.List;
 
 public class PostNGet {
     public static void main(String[] args) throws Exception {
-        PostNGet solution = new PostNGet();
-        solution.sendPost("https://eorvrmrakn8lw4a.m.pipedream.net", "name=zapp&mood=good&locale=&id=777");
+       // PostNGet solution = new PostNGet();
+      //solution.sendPost("https://eorvrmrakn8lw4a.m.pipedream.net", "name=zapp&mood=good&locale=&id=777");
+      // decodeURLString("https://www.amrood.com/index.htm?language=en#j2se");
+        code();
     }
 
     public void sendPost(String url, String urlParameters) throws Exception {
@@ -59,4 +65,58 @@ public class PostNGet {
     protected HttpClient getHttpClient() {
         return HttpClientBuilder.create().build();
     }
+
+    public static void decodeURLString(String s) throws MalformedURLException {
+        try{
+        URL url = new URL(s);
+        System.out.println(url.getProtocol());
+        System.out.println(url.getAuthority());
+        System.out.println(url.getFile());
+        System.out.println(url.getHost());
+        System.out.println(url.getPath());
+        System.out.println(url.getPort());
+        System.out.println(url.getDefaultPort());
+        System.out.println(url.getQuery());
+        System.out.println(url.getRef());}
+        catch (MalformedURLException e){
+            System.out.println("Parameter "+s+" is not a valid URL.");
+        }
+
+
+    }
+
+
+
+
+    public static void code(){
+        try {
+            URL url = new URL("http://jsonplaceholder.typicode.com/posts/1");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2");
+
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Server output .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
