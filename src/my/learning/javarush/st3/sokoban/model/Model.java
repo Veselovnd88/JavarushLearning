eadfd06578470693955043546e9151a71e9ad791
nikolay <veselovnd@gmail.com>
2022-06-09 18:38:26 +0300

    jrushtasks

--- src/my/learning/javarush/st3/sokoban/model/Model.java
+++ src/my/learning/javarush/st3/sokoban/model/Model.java
@@ -1,13 +1,48 @@
 package my.learning.javarush.st3.sokoban.model;
 
+import my.learning.javarush.st3.sokoban.controller.Controller;
 import my.learning.javarush.st3.sokoban.controller.EventListener;
+
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Model {
     private EventListener eventListener;
+    private GameObjects gameObjects;
+    private int currentLevel=1;
+    private LevelLoader levelLoader;
+
     public static final int FIELD_CELL_SIZE = 20;
 
+    public Model() {
+        try {
+            levelLoader = new LevelLoader(Paths.get(getClass().getResource("../res/levels.txt").toURI()));
+        } catch (URISyntaxException e) {
+        }
+    }
     public void setEventListener(EventListener eventListener){
         this.eventListener = eventListener;
     }
+    public GameObjects getGameObjects(){
+        return gameObjects;
+    }
+    public void restartLevel(int level){
+        gameObjects = levelLoader.getLevel(level);
+    }
+    public void restart(){
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel(){
+        currentLevel++;
+        restartLevel(currentLevel);
+    }
+
+
+
+
+
+
 
 }
